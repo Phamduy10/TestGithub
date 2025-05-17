c42a6c7a71776ce3fd765766e882e6d13a936447
Phamduy10 <phamduy10102005@gmail.com>
2025-05-17 16:37:21 +0700

    add string

--- src/java/controller/HomeController.java
+++ src/java/controller/HomeController.java
@@ -25,6 +25,7 @@ public class HomeController extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
                 response.sendRedirect("login.jsp");
+                String a = "name";
     } 
 
    
